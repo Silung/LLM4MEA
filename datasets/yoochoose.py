737f5d7648ff9276a871a8ee88d54f57aa3cdae5
silung <zhaoshilong0108@126.com>
2023-03-02 16:31:29 +0800

    add comments and hints

--- datasets/yoochoose.py
+++ datasets/yoochoose.py
@@ -19,6 +19,10 @@ class YooChooseDataset(AbstractDataset):
     def code(cls):
         return 'yoochoose'
 
+    '''
+    description: 数据集下载链接
+    return {str}
+    '''
     @classmethod
     def url(cls):
         return 'https://s3-eu-west-1.amazonaws.com/yc-rdata/yoochoose-data.7z'
@@ -27,6 +31,9 @@ class YooChooseDataset(AbstractDataset):
     def zip_file_content_is_folder(cls):
         return True
 
+    '''
+    description: 下载数据文件内的文件列表
+    '''
     @classmethod
     def all_raw_file_names(cls):
         return ['dataset-README.txt',
@@ -42,6 +49,11 @@ class YooChooseDataset(AbstractDataset):
     def is_7zfile(cls):
         return True
 
+    '''
+    description: 下载数据，已有数据则跳过
+    param {*} self
+    return {None} 
+    '''
     def maybe_download_raw_dataset(self):
         folder_path = self._get_rawdata_folder_path()
         if folder_path.is_dir() and\
@@ -58,6 +70,11 @@ class YooChooseDataset(AbstractDataset):
             shutil.move(item, folder_path.joinpath(item))
         print()
 
+    '''
+    description: 下载原始数据，去除互动较少的user & item，重新生成Index并划分训练集、验证集和测试集，最后将数据和映射关系保存到字典中序列化
+    param {*} self
+    return {None}
+    '''
     def preprocess(self):
         dataset_path = self._get_preprocessed_dataset_path()
         if dataset_path.is_file():
@@ -78,7 +95,12 @@ class YooChooseDataset(AbstractDataset):
         with dataset_path.open('wb') as f:
             pickle.dump(dataset, f)
 
-    def load_ratings_df(self):
+    '''
+    description: 生成user-item-time-category关系表
+    param {*} self
+    return {pandas.Dataframe}
+    '''
+    def load_ratings_df(self) -> pd.DataFrame:
         folder_path = self._get_rawdata_folder_path()
         file_path = folder_path.joinpath('yoochoose-clicks.dat')
         df = pd.read_csv(file_path, header=None)
