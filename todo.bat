7551f6788043f226982c4383c0d99298b3a6d45f
赵世龙 <1120192517@bit.edu.cn>
2023-03-15 21:33:02 +0800

    add todo.bat

---
+++ todo.bat
@@ -0,0 +1,21 @@
+@REM dataset 'ml-1m', 'ml-20m', 'steam', 'games', 'beauty', 'beauty_dense', 'yoochoose'
+@REM model None, 'bert', 'sas', 'narm'
+
+@REM conda activate cuda
+@REM bert
+python train.py --dataset_code steam --model_code bert
+python train.py --dataset_code yoochoose --model_code bert
+
+@REM sas
+python train.py --dataset_code ml-1m --model_code sas
+python train.py --dataset_code ml-20m --model_code sas
+python train.py --dataset_code steam --model_code sas
+python train.py --dataset_code games --model_code sas
+python train.py --dataset_code beauty --model_code sas
+python train.py --dataset_code beauty_dense --model_code sas
+python train.py --dataset_code yoochoose --model_code sas
+
+@REM narm
+python train.py --dataset_code ml-20m --model_code narm
+python train.py --dataset_code steam --model_code narm
+python train.py --dataset_code yoochoose --model_code narm
