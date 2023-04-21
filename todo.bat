e5b4a332aaf4657e268de3e7bab742e590e9a0d4
silung <zhaoshilong0108@126.com>
2023-04-21 09:56:36 +0800

    update todo

--- todo.bat
+++ todo.bat
@@ -38,8 +38,11 @@ python distill.py --dataset_code beauty_dense --model_code bert --bb_model_code 
 python distill.py --dataset_code games --model_code bert --bb_model_code bert --num_generated_seqs 5000
 python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 5000
 python distill.py --dataset_code ml-1m --model_code narm --bb_model_code narm --num_generated_seqs 5000 --loss myranking --batch_size 2048
-python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 5000 --loss list --batch_size 700
+python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 5000 --loss list --batch_size 800 --num_epochs 1000
 python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 5000 --loss list+neg --batch_size 700
+python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 500 --batch_size 800 --num_epochs 1000
+python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 500 --loss list+neg --batch_size 800 --num_epochs 1000
+python distill.py --dataset_code ml-1m --model_code bert --bb_model_code bert --num_generated_seqs 500 --loss myranking --batch_size 800 --num_epochs 1000
 
 @REM On wk running
 python distill.py --dataset_code beauty --model_code sas --bb_model_code bert --num_generated_seqs 5000
